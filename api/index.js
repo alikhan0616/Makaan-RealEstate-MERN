40a5a2592224e050805e3cd112813377883e0d0b
alikhan0616 <ak098boy@gmail.com>
2025-04-10 20:20:16 +0500

    create update user api

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import cors from 'cors'
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -15,7 +16,10 @@ mongoose.connect(process.env.MONGO).then(() => {
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
+
+app.use(cookieParser());
+
 app.listen(3000, () => {
     console.log("server running on local port 3000")
 })
