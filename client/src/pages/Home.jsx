18de962afa417ca954cda63d164c359e2bf8e41a
alikhan0616 <ak098boy@gmail.com>
2025-04-18 03:24:07 +0500

    fix another type in home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -52,7 +52,7 @@ export default function Home() {
       <div className="flex flex-col gap-6 py-26 px-3 max-w-6xl mx-auto">
         <h1 className='text-slate-700 font-bold text-3xl lg:text-6xl'>Find your next <span className='text-slate-500'>perfect</span><br/> place with ease</h1>
         <div className="text-gray-500 text-xs sm:text-sm">
-          Makaan State will help you find your home fast, early and comfortable. Our expert support are alwways available
+          Makaan State will help you find your home fast, early and comfortable. Our expert support are always available
          <br />
          We have a wide range of properties for you to choose from.
         </div>
